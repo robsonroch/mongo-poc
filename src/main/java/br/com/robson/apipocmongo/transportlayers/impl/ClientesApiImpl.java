b0de26b9b97ab0c42c5f5bf5a3cd50d65c624fd5
Robson <robson.proxycode@gmail.com>
2023-04-12 11:30:48 -0300

    ignore logs folder

---
+++ src/main/java/br/com/robson/apipocmongo/transportlayers/impl/ClientesApiImpl.java
@@ -0,0 +1,10 @@
+package br.com.robson.apipocmongo.transportlayers.impl;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.robson.apipocmongo.transportlayers.openapi.api.ClientesApi;
+
+@RestController
+public class ClientesApiImpl implements ClientesApi{
+
+}
